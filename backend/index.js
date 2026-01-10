eaa45f09add7c0fbe0adedf1635e65afac7ccbc8
VishalSaini7272 <sainivishal156@gmail.com>
2026-01-10 22:44:26 +0530

    index.js code edit

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,12 @@ dotenv.config()
 const app = express();
 
 //middleware:-
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
+
 app.use(express.json())
 
 
